b6d7e027f0cb72e47f22448fa1fbc00751ddcffd
Daniele Ognibene <danieleognibene@MacBook-Pro-di-AdminOD-3.local>
2021-04-01 14:12:55 +0200

    Add splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    document.getElementsByClassName('splash-screen').item(0).classList.add('hidden');
+  }
+
+}
